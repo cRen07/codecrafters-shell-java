0d38b95cd0f6c97707c5d1d2ea278dbc8283f256
cRen07 <cchoi3357@gmail.com>
2024-06-26 22:27:13 -0400

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,16 @@
+//When opening a new terminal when you open this project for the first time make sure the terminal is in the correct directory 
+// PS C:\Users\Caleb Choi\Desktop\Projects\Shell Project (Java)\codecrafters-shell-java>
+//If not use the command cd to change the directory to the correct one
+//'cd codecrafters-shell-java' <-- USE THIS COMMAND TO GET TO THE CORRECT DIRECTORY AND TO BE ABLE TO USE GIT COMMANDS
+
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
-        
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        System.out.println(input + " command not found")
     }
 }
