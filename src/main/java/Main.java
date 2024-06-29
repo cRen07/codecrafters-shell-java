c125e8a6ed1f85aa50bc50ecbe82776ce751a452
cRen07 <cchoi3357@gmail.com>
2024-06-29 16:23:03 -0400

    REPL Stage Part 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to the shell");
         Scanner scanner = new Scanner(System.in);
         System.out.print("$ ");
         String input = scanner.nextLine();
