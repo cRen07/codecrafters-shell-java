88fd979cbcbe80c3cbf73ef505ceda86dbe78ae7
cRen07 <cchoi3357@gmail.com>
2024-07-02 18:33:24 -0400

    Path step: refactored code part 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,16 @@
-//When opening a new terminal when you open this project for the first time make sure the terminal is in the correct directory 
-// PS C:\Users\Caleb Choi\Desktop\Projects\Shell Project (Java)\codecrafters-shell-java>
-//If not use the command cd to change the directory to the correct one
-//'cd codecrafters-shell-java' <-- USE THIS COMMAND TO GET TO THE CORRECT DIRECTORY AND TO BE ABLE TO USE GIT COMMANDS
-
+import java.io.File;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+
+        String[] builtins = {"echo", "exit", "type"};
+
         while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
-            String[] builtins = {"cd", "echo", "exit", "type"};
             String[] parts = input.split(" ");
             String command = parts[0];
             String[] arguments = Arrays.copyOfRange(parts, 1, parts.length);
@@ -21,30 +19,51 @@ public class Main {
                 case "exit":
                     return;
                 case "cd":
-                break;
+                    // Add your "cd" command logic here
+                    break;
                 case "echo":
+                    // Concatenate the arguments
                     StringBuilder sb = new StringBuilder();
-                    for (String arg : arguments){
+                    for (String arg : arguments) {
                         sb.append(arg).append(" ");
                     }
+                    // Print the result
                     System.out.println(sb.toString().trim());
                     break;
-
                 case "type":
-                    if (arguments.length == 0) {
-                        System.out.println("type: missing argument");
+                    if (arguments.length != 1) {
+                        System.out.println("Usage: type <command>");
+                        break;
+                    }
+                    String commandToCheck = arguments[0];
+                    boolean isBuiltin = false;
+                    for (String builtin : builtins) {
+                        if (builtin.equals(commandToCheck)) {
+                            isBuiltin = true;
+                            break;
+                        }
+                    }
+                    if (isBuiltin) {
+                        System.out.println(commandToCheck + " is a shell builtin");
                     } else {
-                        String argument = arguments[0];
-                        if (Arrays.asList(builtins).contains(argument)) {
-                            System.out.println(argument + " is a shell builtin");
-                        } else {
-                            System.out.println(argument + " not found");
+                        String path = System.getenv("PATH");
+                        String[] pathDirs = path.split(System.getProperty("path.separator"));
+                        boolean found = false;
+                        for (String dir : pathDirs) {
+                            File file = new File(dir, commandToCheck);
+                            if (file.isFile() && file.canExecute()) {
+                                System.out.println(commandToCheck + " is " + file.getAbsolutePath());
+                                found = true;
+                                break;
+                            }
+                        }
+                        if (!found) {
+                            System.out.println(commandToCheck + ": not found");
                         }
                     }
                     break;
                 default:
                     System.out.println(input + ": command not found");
-
             }
         }
     }
