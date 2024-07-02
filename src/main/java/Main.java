9674a62612c147797cb723c4736b59169a2d5b1a
cRen07 <cchoi3357@gmail.com>
2024-07-02 19:18:58 -0400

    pwd builtin part 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,8 @@
+//When opening a new terminal when you open this project for the first time make sure the terminal is in the correct directory 
+// PS C:\Users\Caleb Choi\Desktop\Projects\Shell Project (Java)\codecrafters-shell-java>
+//If not use the command cd to change the directory to the correct one
+//'cd codecrafters-shell-java' <-- USE THIS COMMAND TO GET TO THE CORRECT DIRECTORY AND TO BE ABLE TO USE GIT COMMANDS
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
